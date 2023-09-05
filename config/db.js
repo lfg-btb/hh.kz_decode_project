0214c40ccfc1736640c279c7a55a31261a425ba4
lfg-btb <beyself@vk.com>
2023-09-05 13:37:51 +0600

    production mode config

--- config/db.js
+++ config/db.js
@@ -1,13 +1,21 @@
 const {Sequelize} = require('sequelize')
 const dbConf = require('./config')
+const fs = require('fs')
+const path = require('path')
 
 let sequelize; 
 
-if(process.env === "production"){
+if(process.env.NODE_ENV === "production"){
+    console.log("Hello");
     sequelize = new Sequelize(dbConf.production.database, dbConf.production.username, dbConf.production.password, {
         host: dbConf.production.host,
         dialect: dbConf.production.dialect,
-        port: dbConf.production.port
+        port: dbConf.production.port,
+        dialectOptions: {
+            ssl:{
+                ca: fs.readFileSync(path.resolve("config", "ca-certificate.crt"))
+            }
+        }
     })
 }else{
     sequelize = new Sequelize(dbConf.development.database, dbConf.development.username, dbConf.development.password, {
