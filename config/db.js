ba6581e0f6dc89e5668dd400c27f33303d102145
lfg-btb <beyself@vk.com>
2023-09-08 08:56:23 +0600

    prod config edit

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const {Sequelize} = require('sequelize')
 const dbConf = require('./config')
 const fs = require('fs')
-const path = require('path')
+const path = require('path');
 
 let sequelize; 
 
 if(process.env.NODE_ENV === "production"){
-    console.log(dbConf.production);
     sequelize = new Sequelize(dbConf.production.database, dbConf.production.username, dbConf.production.password, {
         host: dbConf.production.host,
         dialect: dbConf.production.dialect,
@@ -18,6 +17,7 @@ if(process.env.NODE_ENV === "production"){
         }
     })
 }else{
+    
     sequelize = new Sequelize(dbConf.development.database, dbConf.development.username, dbConf.development.password, {
         host: dbConf.development.host,
         dialect: dbConf.development.dialect,
@@ -33,6 +33,7 @@ sequelize
         console.log('Connection to the database has been established successfully');
     })
     .catch((error) =>{
+        console.log(process.env.NODE_ENV);
         console.error('Unable to connect to the database', error);
     })
 
